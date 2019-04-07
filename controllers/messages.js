4e971663ce9c3c964cc2d582335cb8ad2ff9de7c
Taylor Hurt <taylor.a.hurt@gmail.com>
2019-04-07 19:47:03 -0400

    add error responses for messages and fixed hanging controller responses
    hanging responses were being caused by some endpoints not including a
    try/catch clause, so no response was being sent back to the client.

--- controllers/messages.js
+++ controllers/messages.js
@@ -22,33 +22,59 @@ router
   })
   // read all messages
   .get("/", async (req, res) => {
-    const messages = await Message.findAll({
-      include: [Like],
-      limit: req.query.limit || 100,
-      offset: req.query.offset || 0,
-      order: [["createdAt", "DESC"]]
-    });
-    res.json({ messages });
+    try {
+      const messages = await Message.findAll({
+        include: [Like],
+        limit: req.query.limit || 100,
+        offset: req.query.offset || 0,
+        order: [["createdAt", "DESC"]]
+      });
+      res.json({ messages });
+    } catch (err) {
+      console.error(err);
+      if (err instanceof Sequelize.ValidationError) {
+        return res.status(400).send({ errors: err.errors });
+      } else if (err instanceof Sequelize.DatabaseError) {
+        return res.status(400).send({ error: err.toString() });
+      }
+      res.status(500).send();
+    }
   })
   // read message by id
   .get("/:id", async (req, res) => {
-    const message = await Message.findById(req.params.id, {
-      include: [Like]
-    });
-    res.json({ message });
+    try {
+      const message = await Message.findById(req.params.id, {
+        include: [Like]
+      });
+      res.json({ message });
+    } catch (err) {
+      console.error(err);
+      if (err instanceof Sequelize.DatabaseError) {
+        return res.status(400).send({ error: err.toString() });
+      }
+      return res.status(500).send();
+    }
   })
   // delete message
   .delete("/:id", authMiddleware, async (req, res) => {
-    const destroyedCount = await Message.destroy({
-      where: {
-        id: req.params.id,
-        userId: req.user.id
+    try {
+      const destroyedCount = await Message.destroy({
+        where: {
+          id: req.params.id,
+          userId: req.user.id
+        }
+      });
+      if (destroyedCount === 0) {
+        return res.status(404).send({ error: "Message does not exist" });
       }
-    });
-    if (destroyedCount === 0) {
-      return res.status(404).send({ error: "Message does not exist" });
+      res.json({ id: req.params.id });
+    } catch (err) {
+      console.error(err);
+      if (err instanceof Sequelize.DatabaseError) {
+        return res.status(400).send({ error: err.toString() });
+      }
+      return res.status(500).send();
     }
-    res.json({ id: req.params.id });
   });
 
 module.exports = router;
