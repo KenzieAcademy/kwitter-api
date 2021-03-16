6e32d6267be5439427fce6e715c8b0f9b33365b1
Peter Mayor <petem14@gmail.com>
2021-03-16 17:24:27 -0400

    back to sequelize 4.44, add ssl to sequelize

--- models/index.js
+++ models/index.js
@@ -1,7 +1,10 @@
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  operatorsAliases: false
+  operatorsAliases: false,
+  dialectOptions: {
+    ssl: true
+  }
 });
 
 // import models into sequelize
